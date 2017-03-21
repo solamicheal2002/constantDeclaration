f6aa8059e72d333f6a5ba131d0efdcc7ea44cdda
Sola Micheal <solamicheal2002@gmail.com>
2017-03-21 16:17:01 +0100

    initial commit

---
+++ constantDeclaration/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constantDeclaration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const double PI = 3.14952;
+
+           
+            Console.WriteLine("Enter value of Radius");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Double AreaCircle = PI* r * r;
+            Console.WriteLine("Radius: {0},AreaCircle is: {1},PI: {2}", r, AreaCircle, PI);
+            Console.ReadLine();
+        }
+    }
+}
